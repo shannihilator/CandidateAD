e7e764fc61a7d40991314e4219768547b9c04033
anonymous <anonymous>
2018-11-25 10:57:09 -0500

    Wires up server to port 3500 and runs first route to be sure everything is hooked up correctly and running. Success.

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 //IMPORT EXPRESS
 const express = require("express");
 const app = express();
+
+//TEST
+app.get("/", (req, res) => {
+  res.send("Homepage Works!");
+});
+
+//=====================================================
+app.listen(3500, () => {
+  console.log("Your server is running on: 3500!");
+});
