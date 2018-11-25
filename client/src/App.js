f2c8aec803f8185a5d569cd7b8a905205214c562
anonymous <anonymous>
2018-11-25 13:42:41 -0500

    adds react-router-dom for toggling pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // COMPONENTS
 import Navigation from "./components/Navigation";
@@ -9,10 +10,12 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navigation />
-        <h1>TODO: Mainbody</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navigation />
+          <h1>TODO: Mainbody</h1>
+        </div>
+      </Router>
     );
   }
 }
