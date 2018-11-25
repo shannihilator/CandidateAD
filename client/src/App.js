7b25abface3bd3475956146448fc5e0d5bde711c
anonymous <anonymous>
2018-11-25 13:17:00 -0500

    adds initial creation of Navigation component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+// COMPONENTS
+import Navigation from "./components/Navigation";
+
+// CSS
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navigation />
+        <h1>TODO: Mainbody</h1>
       </div>
     );
   }
