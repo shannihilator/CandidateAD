b08c03ab8ce4d90e6490d779543d8edade65da93
anonymous <anonymous>
2018-11-25 13:47:29 -0500

    creates initial component for AllUsers

---
+++ client/src/components/AllUsers.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AllUsers extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DISPLAYS ALL USERS</h1>
+      </div>
+    );
+  }
+}
+
+export default AllUsers;
