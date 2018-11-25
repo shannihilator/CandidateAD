684434d529546d7336328aa9458b48d9772a926f
anonymous <anonymous>
2018-11-25 15:48:31 -0500

    recieves props and draws to pages

---
+++ client/src/components/OneUser.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class OneUser extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.props.userName}</p>
+        <p>{this.props.jobTitle}</p>
+        <p>{this.props.email}</p>
+      </div>
+    );
+  }
+}
+
+export default OneUser;
