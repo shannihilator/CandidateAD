58cb45105e5c588f663ac959dcd6492f480385b2
anonymous <anonymous>
2018-11-26 10:23:06 -0500

    adds comment for description of Components function

--- client/src/components/Frequency.js
+++ client/src/components/Frequency.js
@@ -1,23 +1,16 @@
 import React, { Component } from "react";
 import OneFrequency from "./OneFrequency";
-
-// Create a button that, when clicked, displays a frequency count of all the unique characters in all the email addresses of all the People you have access to, sorted by frequency count.
-
+//Component to Hold All email Objects
 class Frequency extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    // console.log(this.props.emails);
-    //Loops over email and pulls out keys
+    //Loops over email and pulls out entries
     let entries = Object.entries(this.props.emails).map(k => {
       return <OneFrequency key={k[0]} letter={k[0]} number={k[1]} />;
     });
 
     return (
       <div>
-        <h1>TODO: SHOW FREQUENCY OF LETTERS FROM USERS</h1>
+        <h1>FREQUENCY OF LETTERS FROM ALL USERS EMAILS</h1>
         {entries}
       </div>
     );
